925025a52b8d48d6cc29bfe25a7d2f4477b3f3f0
Timush Odilla <108368693+spelsinx@users.noreply.github.com>
2022-11-24 17:39:06 +0500

    Change entry

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin"); 
   
  module.exports = { 
-   entry: "./src/index.js", 
+   entry: "./src/App.jsx", 
    output: { 
      filename: "main.js", 
      path: path.resolve(__dirname, "build"), 
